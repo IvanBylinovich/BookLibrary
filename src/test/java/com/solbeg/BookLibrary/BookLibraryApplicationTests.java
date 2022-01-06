e7ba65ab5854f101693b5cfe792e52c539d12567
ivan.bylinovich <ib6020607@gmail.com>
2022-01-06 18:53:46 +0300

    Init commit

---
+++ src/test/java/com/solbeg/BookLibrary/BookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solbeg.BookLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
