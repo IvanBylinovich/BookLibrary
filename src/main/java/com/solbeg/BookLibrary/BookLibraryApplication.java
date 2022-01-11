1084bf86cb1a048d31fc75b861ffe4946a9cf5c7
ivan.bylinovich <ib6020607@gmail.com>
2022-01-11 11:05:00 +0300

    Added Swagger docs and ui

--- src/main/java/com/solbeg/BookLibrary/BookLibraryApplication.java
+++ src/main/java/com/solbeg/BookLibrary/BookLibraryApplication.java
@@ -2,12 +2,17 @@ package com.solbeg.BookLibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class BookLibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookLibraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookLibraryApplication.class, args);
+    }
 
 }
